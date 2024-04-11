bd980884650d44fa0623149a3b7c273a74fc4f4d
TejashPatel9654 <pateltejash12379@gmail.com>
2024-04-11 23:16:04 +0530

    add checkout

--- public/user/update_cart_session.php
+++ public/user/update_cart_session.php
@@ -2,8 +2,12 @@
 session_start();
 
 if (isset($_POST['cart'])) {
-    $old = $_SESSION['carts'];
-    $_SESSION['carts'] = array_merge($old,$_POST['cart']);
+    if($_SESSION['cart']){
+        $old = $_SESSION['cart'];
+        $_SESSION['carts'] = array_merge($old,$_POST['cart']);
+    }else{
+        $_SESSION['carts'] = $_POST['cart'];
+    }
     echo "Cart updated successfully!";
 } else {
     echo "Error: Cart data not received.";
